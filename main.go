9672af2d62e0df47075de4a4ec94461cb099a3c4
Yuya Minami <yuya373@me.com>
2019-01-08 13:27:36 +0900

    add repl

--- main.go
+++ main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"github.com/yuya373/monkey/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf(
+		"Hello %s! this is the Monkey programming language!\n",
+		user.Username,
+	)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
